d6bd4b027e421276234dfeea8e7c4369be233f8f
Enfu Liao <enfu.liao.cs10@nycu.edu.tw>
2024-03-22 12:14:45 +0800

    feat: Hide room color for regular user
    普通使用者不需要看到琴房的顏色代號

--- app/helpers/rooms_helper.rb
+++ app/helpers/rooms_helper.rb
@@ -1,2 +1,16 @@
 module RoomsHelper
+    def row(room)
+        if current_user.regular?
+            render partial: "regular_row", locals: { room: room }
+        elsif current_user.admin?
+            render partial: "admin_row", locals: { room: room }
+        end
+    end
+    def head
+        if current_user.regular?
+            render partial: "regular_head"
+        elsif current_user.admin?
+            render partial: "admin_head"
+        end
+    end
 end
